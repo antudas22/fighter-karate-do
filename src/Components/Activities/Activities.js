c1416a767ea0f7b38ea79dbfe791561f7bb921e6
antudas22 <20antu20@gmail.com>
2022-09-29 01:34:26 +0600

    Logo and title part finished

---
+++ src/Components/Activities/Activities.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './Activities.css';
+
+const Activities = () => {
+    return (
+        <div className='home'>
+            <div className="activity-container">
+                <div className="logo">
+                    <h1><i class="fa-solid fa-user-ninja"></i> Fighter Karate Do</h1>
+                </div>
+            </div>
+            <div className="list-container">
+            </div>
+        </div>
+    );
+};
+
+export default Activities;
