8b47c693c50a064a98dfc5f05058afad9582d229
antudas22 <20antu20@gmail.com>
2022-09-29 16:51:21 +0600

    list section customization completed

--- src/Components/Activities/Activities.js
+++ src/Components/Activities/Activities.js
@@ -3,6 +3,7 @@ import { faUserNinja } from '@fortawesome/free-solid-svg-icons'
 import React, { useEffect, useState } from 'react';
 import Task from '../Task/Task';
 import './Activities.css';
+import List from '../List/List';
 
 const Activities = () => {
     const [activities, setActivities] = useState([]);
@@ -39,16 +40,9 @@ const Activities = () => {
                 </div>
             </div>
             <div className="list-container">
-                <div className="user-info">
-                    <div className="user-img">
-                    <img src="https://media.istockphoto.com/photos/shot-of-a-handsome-young-man-standing-against-a-grey-background-picture-id1335941248?b=1&k=20&m=1335941248&s=170667a&w=0&h=sn_An6VRQBtK3BuHnG1w9UmhTzwTqM3xLnKcqLW-mzw=" alt="" />
-                    </div>
-                    <div className="user-details">
-                        <h2>Jack Juck</h2>
-                        <p>Berlin, Germany</p>
-                    </div>
-                </div>
-                <p>Selected List: {list.length}</p>
+                <List
+                    list={list}
+                ></List>
             </div>
         </div>
     );
---
+++ src/Components/List/List.js
@@ -0,0 +1,47 @@
+import React from 'react';
+import './List.css';
+
+const List = (props) => {
+    const {list} = props;
+    
+    let totalTime = 0;
+    for(const item of list){
+        totalTime = totalTime + item.time;
+    }
+    return (
+        <div className='list'>
+            <div className="user-info">
+                    <div className="user-img">
+                        <img src="https://media.istockphoto.com/photos/shot-of-a-handsome-young-man-standing-against-a-grey-background-picture-id1335941248?b=1&k=20&m=1335941248&s=170667a&w=0&h=sn_An6VRQBtK3BuHnG1w9UmhTzwTqM3xLnKcqLW-mzw=" alt="" />
+                    </div>
+                    <div className="user-details">
+                        <h2>Jack Juck</h2>
+                        <p>Berlin, Germany</p>
+                    </div>
+                </div>
+                <div className='user-another-details'>
+                    <div><p>Weight: 60kg</p></div>
+                    <div><p>Height: 5.7</p></div>
+                    <div><p>Age: 20yrs</p></div>
+                </div>
+
+                <div>
+                    <h3>Time Break</h3>
+                    <div className='time-break'>
+                        <button>1m</button>
+                        <button>3m</button>
+                        <button>5m</button>
+                        <button>10m</button>
+                    </div>
+                </div>
+
+                <h3>Exercise Details</h3>
+                <p className='time'>Exercise Time: {totalTime} Minutes</p>
+                <p className='break'>Break Time:</p>
+
+                <button className='completed-btn'>Activity Completed</button>
+        </div>
+    );
+};
+
+export default List;
