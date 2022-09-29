a1a7ddff2d862f213f5c80e3f1679cd5df7f4cd0
antudas22 <20antu20@gmail.com>
2022-09-29 12:32:44 +0600

    completed card section

--- src/Components/Task/Task.js
+++ src/Components/Task/Task.js
@@ -6,9 +6,13 @@ const Task = (props) => {
     return (
         <div className='task-card'>
             <img src={img} alt="" />
-            <h2 className='task-name'>{name}</h2>
-            <h4>Age: {age}</h4>
-            <h4>Time required: {time}m</h4>
+            <div className="task-details">
+                <h2 className='task-name'>{name}</h2>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum dignissimos autem nemo odio.</p>
+                <h4>Age: {age}</h4>
+                <h4>Time required: {time}m</h4>
+            </div>
+            <button className='card-btn'>Add to list</button>
         </div>
     );
 };
